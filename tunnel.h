2a4267fdeb3513663957cbc3cc234cd510896ce1
Matvey Kukuy <motakuK@gmail.com>
2013-02-03 10:33:50 -0800

    Готов заголовок в классе туннеля

--- tunnel.h
+++ tunnel.h
@@ -1,10 +1,36 @@
 #ifndef TUNNEL_H
 #define TUNNEL_H
 
+//Содержит в себе данные о туннеле
+
+#include <QString>
+#include <QVector>
+
+#include <knownpeer.h>
+
 class tunnel
 {
+private:
+    //Необходимое колличество роутеров:
+    int needRoutersNumber;
+
+    //Массив данных о роутерах:
+    QVector<KnownPeer* > reuters;
+
 public:
     tunnel();
+
+    //Добавить данные об известном роутере:
+    void addRouterData(KnownPeer*);
+
+    //Получить данные о роутере по номеру:
+    KnownPeer* getRouter(int number);
+
+    //Получить колличество роутеров:
+    int getRoutersNumber();
+
+    //Получить необходимое колличество роутеров:
+    int getNeedRoutersNumber();
 };
 
 #endif // TUNNEL_H
